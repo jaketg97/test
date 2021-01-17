4b80fab1285fffdd68d9b60c12e6fb8e64c736bd
jaketg97 <jaketg97@gmail.com>
2021-01-16 22:00:11 -0600

    yay

--- aca_competition_consolidation/r_code/build_hospital_hhi.R
+++ aca_competition_consolidation/r_code/build_hospital_hhi.R
@@ -1,10 +1,30 @@
+##################################################
+# Calling libraries
+##################################################
 library(readxl)
-library(maps)
+library(tigris)
 
-x <- read_excel("/Volumes/GoogleDrive/My Drive/2018-2019/BA Thesis/Data/Data Cleaning/AHA Data/AHA_2014.xlsx")
-crosswalk <- read_excel("Enrollment Data/crosswalk.xlsx")
-colnames(crosswalk)<-c("county_code", "rating_area")
+data("fips_codes")
+
+#################################################
+# Generate 2014 Hospital HHI (hospital_hhi_2014)
+#################################################
+
+
+x <- read_excel("AHA_2014.xlsx")
+x$county <- gsub("(.*),.*", "\\1", x$`Hospital's County name`)
+x$county <- paste(x$county, "County", sep=" ")
+x <- merge(x, fips_codes, by.x = c("State (physical)", "county"), by.y = c("state", "county"))
+x$county_code <- paste(x$state_code, x$county_code, sep = "")
+
+crosswalk <- read_excel("ratingarea_county_crosswalk.xlsx", sheet = "temp", col_types = c("text", "text", "text", "text", "text"))
+#crosswalk$county_code <- as.integer(crosswalk$county_code)
+crosswalk <- as.data.frame(cbind(crosswalk$ratingarea, crosswalk$county_code, crosswalk$state_fips))
+colnames(crosswalk) <- c("rating_area", "county_code", "state_fips")
+
 x <- merge(x, crosswalk, "county_code")
+x$rating_area <- paste(x$state_code, x$rating_area, sep = "") #marking rating area with state fips, to prevent repeats
+
 x$id_1 <- ifelse(is.na(x$`System ID`), x$`AHA ID`, x$`System ID`)
 x$id_2 <-paste(x$id_1, x$rating_area)
 y <- aggregate(x$Admissions, by=list(x$id_2), FUN=sum, na.rm=TRUE)
@@ -17,9 +37,14 @@ colnames(y) <- c("rating_area", "county_admissions")
 z <- merge(z, y, "rating_area")
 z$mkt_share_squared <- ((z$Admissions/z$county_admissions)*100)^2
 hospital_hhi_2014 <- aggregate(z$mkt_share_squared, by=list(z$rating_area), FUN=sum, na.rm=TRUE)
-colnames(hospital_hhi_2014) <- c("rating_area", "hospital_hhi")
-hospital_hhi_2014 <-merge(crosswalk, hospital_hhi_2014, "rating_area")
-colnames(hospital_hhi_2014)<-c("rating_area", "fips", "hospital_hhi")
+colnames(hospital_hhi_2014) <- c("state_fip_rating_area", "hospital_hhi")
+
+crosswalk$state_fip_rating_area <- paste(crosswalk$state_fips, crosswalk$rating_area, sep = "")
+hospital_hhi_2014 <-merge(crosswalk, hospital_hhi_2014, "state_fip_rating_area")
+
+#################################################
+# Generate 2015 Hospital HHI (hospital_hhi_2015)
+#################################################
 
 x <- read_excel("/Volumes/GoogleDrive/My Drive/2018-2019/BA Thesis/Data/Data Cleaning/AHA Data/AHA_2015.xlsx")
 crosswalk <- read_excel("Enrollment Data/crosswalk.xlsx")
--- aca_competition_consolidation/r_code/build_insurer_hhi.R
+++ aca_competition_consolidation/r_code/build_insurer_hhi.R
@@ -1,12 +1,26 @@
+#################################################
+# Calling libraries
+#################################################
 library(readxl)
-library(ggplot2)
-library(maps)
+library(tidyverse)
 
-x <- read_excel("/Volumes/GoogleDrive/My Drive/2018-2019/BA Thesis/Data/Data Cleaning/Enrollment Data/enrollment_2015_abb.xlsx")
-colnames(x) <- c("state", "issuer_id", "county_code", "enrollment_count","state fip","rating_area_local","rating_area")
+#################################################
+# Generate 2015 Insurer HHI (insurer_hhi_2015)
+#################################################
+crosswalk <- read_excel("ratingarea_county_crosswalk.xlsx", sheet = "temp", col_types = c("text", "text", "text", "text", "text"))
+#crosswalk$county_code <- as.integer(crosswalk$county_code)
+crosswalk <- as.data.frame(cbind(crosswalk$ratingarea, crosswalk$county_code, crosswalk$state_fips))
+colnames(crosswalk) <- c("rating_area", "county_code", "state_fips")
+
+x <- read_excel("2015-Issuer-Data-Final_.xlsx", sheet = "2015 Issuer")
+x <- as.data.frame(cbind(x$issuer_hios_id, x$tenant_id, x$plcy_county_fips_code, x$ever_enrolled_plan_sel))
+colnames(x) <- c("issuer_id", "state", "county_code", "enrollment_count")
+x <- merge(crosswalk, x, by = "county_code")
+x$rating_area <- paste(x$state_fips, x$rating_area, sep = "")
+
 x <- subset(x, x$state!="AK" & x$state!="NE")
 x$enrollment_count <- as.integer(x$enrollment_count)
-x$id <- paste(x$rating_area_local, x$issuer_id)
+x$id <- paste(x$rating_area, x$issuer_id)
 x_1 <- aggregate(x$enrollment_count, by=list(x$id), FUN=sum, na.rm=TRUE)
 colnames(x_1) <- c("id", "enrollment_count")
 x_2 <- merge(x_1, x, "id")
@@ -15,13 +29,28 @@ colnames(y) <- c("rating_area", "enrollment_count")
 z<-merge(x_2, y, "rating_area")
 z$mkt_share_squared <- ((z$enrollment_count.x/z$enrollment_count)*100)^2
 insurer_hhi_2015 <- aggregate(z$mkt_share_squared, by=list(z$rating_area), FUN=sum, na.rm=TRUE)
-colnames(insurer_hhi_2015) <- c("rating_area", "insurer_hhi")
+colnames(insurer_hhi_2015) <- c("state_fip_rating_area", "insurer_hhi")
 
-x <- read_excel("/Volumes/GoogleDrive/My Drive/2018-2019/BA Thesis/Data/Data Cleaning/Enrollment Data/enrollment_2016_abb.xlsx")
-colnames(x) <- c("state", "issuer_id", "county_code", "enrollment_count","state fip","rating_area_local","rating_area")
+crosswalk$state_fip_rating_area <- paste(crosswalk$state_fips, crosswalk$rating_area, sep = "")
+insurer_hhi_2015 <- merge(crosswalk, insurer_hhi_2015, by = "state_fip_rating_area")
+
+#################################################
+# Generate 2016 Insurer HHI (insurer_hhi_2016)
+#################################################
+crosswalk <- read_excel("ratingarea_county_crosswalk.xlsx", sheet = "temp", col_types = c("text", "text", "text", "text", "text"))
+#crosswalk$county_code <- as.integer(crosswalk$county_code)
+crosswalk <- as.data.frame(cbind(crosswalk$ratingarea, crosswalk$county_code, crosswalk$state_fips))
+colnames(crosswalk) <- c("rating_area", "county_code", "state_fips")
+
+x <- read_excel("2016-Enrollment-Disenrollment-Report.xlsx", sheet = "M2_S")
+x <- as.data.frame(cbind(x$`HIOS ID`, x$`Tenant ID`, x$`Policy County FIPS Code`, x$`Ever Enrolled Count`))
+colnames(x) <- c("issuer_id", "state", "county_code", "enrollment_count")
+x <- merge(crosswalk, x, by = "county_code")
+x$rating_area <- paste(x$state_fips, x$rating_area, sep = "")
+
 x <- subset(x, x$state!="AK" & x$state!="NE")
 x$enrollment_count <- as.integer(x$enrollment_count)
-x$id <- paste(x$rating_area_local, x$issuer_id)
+x$id <- paste(x$rating_area, x$issuer_id)
 x_1 <- aggregate(x$enrollment_count, by=list(x$id), FUN=sum, na.rm=TRUE)
 colnames(x_1) <- c("id", "enrollment_count")
 x_2 <- merge(x_1, x, "id")
@@ -30,11 +59,10 @@ colnames(y) <- c("rating_area", "enrollment_count")
 z<-merge(x_2, y, "rating_area")
 z$mkt_share_squared <- ((z$enrollment_count.x/z$enrollment_count)*100)^2
 insurer_hhi_2016 <- aggregate(z$mkt_share_squared, by=list(z$rating_area), FUN=sum, na.rm=TRUE)
-colnames(insurer_hhi_2016) <- c("rating_area", "insurer_hhi")
-crosswalk <- read_excel("Enrollment Data/crosswalk.xlsx")
-colnames(crosswalk)<-c("fips", "rating_area")
+colnames(insurer_hhi_2016) <- c("state_fip_rating_area", "insurer_hhi")
 
-insurer_hhi_2015<-merge(crosswalk, insurer_hhi_2015, "rating_area")
-insurer_hhi_2016<-merge(crosswalk, insurer_hhi_2016, "rating_area")
+crosswalk$state_fip_rating_area <- paste(crosswalk$state_fips, crosswalk$rating_area, sep = "")
+insurer_hhi_2016 <- merge(crosswalk, insurer_hhi_2016, by = "state_fip_rating_area")
 
+rm(x, x_1, x_2, y, z, crosswalk)
 
