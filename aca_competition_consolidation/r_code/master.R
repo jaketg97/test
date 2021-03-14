6d343f4bbfde06b3a1f995e493a2d52cee765e38
jaketg97 <jaketg97@gmail.com>
2021-03-14 12:26:28 -0500

    submit

--- aca_competition_consolidation/r_code/master.R
+++ aca_competition_consolidation/r_code/master.R
@@ -200,6 +200,8 @@ write.csv(full_combined, "../clean_data/full_combined.csv", row.names = FALSE)
 # Running linear models
 #################################################
 library(car)
+library(tidyverse)
+library(data.table)
 rm(list=ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd("../raw_data")
@@ -211,34 +213,36 @@ full_combined$year_dummy <- ifelse(full_combined$year == 2016, 1, 0)
 full_combined$rucc.f <- Recode(full_combined$rucc_code_13, "1:3 = '1-3'; 4:6 = '4-6'; 7:9 = '7-9'")
 full_combined$no_hospitals <- as.integer(full_combined$no_hospitals)
 full_combined$county_code.f <- factor(full_combined$county_code)
+full_combined$ra_year.f <- as.factor(paste(full_combined$rating_area_state, full_combined$year, sep = " "))
+full_combined$st_year.f <- as.factor(paste(full_combined$state_abb, full_combined$year, sep =" "))
 
-model_full <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
-model_full_1 <- lm(insurer_hhi_logged~hospital_hhi_logged_1+no_hospitals_1+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
-model_full_2 <- lm(insurer_hhi_logged~hospital_hhi_logged_2+no_hospitals_2+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
+model_full <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
+model_full_1 <- lm(insurer_hhi_logged~hospital_hhi_logged_1+no_hospitals_1+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
+model_full_2 <- lm(insurer_hhi_logged~hospital_hhi_logged_2+no_hospitals_2+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
 
-model_full_south <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="South"))
-model_full_northcentral <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="North Central"))
-model_full_northeast <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="Northeast"))
-model_full_west <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="West"))
+model_full_south <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="South"))
+model_full_northcentral <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="North Central"))
+model_full_northeast <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="Northeast"))
+model_full_west <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & region.f=="West"))
 
-model_full_13 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="1-3"))
-model_full_46 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="4-6"))
-model_full_79 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="7-9"))
+model_full_13 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="1-3"))
+model_full_46 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="4-6"))
+model_full_79 <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE & rucc.f=="7-9"))
 
-model_stateFE <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+state_abb*year_dummy+medicare_pc, data=full_combined)
-model_stateFE_1 <- lm(insurer_hhi_logged~hospital_hhi_logged_1+no_hospitals_1+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+state_abb*year_dummy+medicare_pc, data=full_combined)
-model_stateFE_2 <- lm(insurer_hhi_logged~hospital_hhi_logged_2+no_hospitals_2+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+state_abb*year_dummy+medicare_pc, data=full_combined)
+model_stateFE <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+st_year.f+year_dummy+medicare_pc, data=full_combined)
+model_stateFE_1 <- lm(insurer_hhi_logged~hospital_hhi_logged_1+no_hospitals_1+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+st_year.f+year_dummy+medicare_pc, data=full_combined)
+model_stateFE_2 <- lm(insurer_hhi_logged~hospital_hhi_logged_2+no_hospitals_2+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+st_year.f+year_dummy+medicare_pc, data=full_combined)
 
 model_noFE <- lm(insurer_hhi_logged~hospital_hhi_logged+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+medicare_pc+mlr+state_govt+medicaid_expansion+year_dummy, data = full_combined)
 model_noFE_1 <- lm(insurer_hhi_logged~hospital_hhi_logged_1+no_hospitals_1+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+medicare_pc+mlr+state_govt+medicaid_expansion+year_dummy, data = full_combined)
 model_noFE_2 <- lm(insurer_hhi_logged~hospital_hhi_logged_2+no_hospitals_2+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+medicare_pc+mlr+state_govt+medicaid_expansion+year_dummy, data = full_combined)
 
-model_full_region <- lm(insurer_hhi_logged~hospital_hhi_logged+hospital_hhi_logged:region.f+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
-model_full_rurality <-lm(insurer_hhi_logged~hospital_hhi_logged+hospital_hhi_logged:rucc.f+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
+model_full_region <- lm(insurer_hhi_logged~hospital_hhi_logged+hospital_hhi_logged:region.f+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
+model_full_rurality <-lm(insurer_hhi_logged~hospital_hhi_logged+hospital_hhi_logged:rucc.f+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
 
-model_num_full <- lm(num_insurers~num_hospitals+num_hospitals*year_dummy+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+rating_area.f*year_dummy+medicare_pc, data=subset(full_combined, single_county_RA==FALSE))
-model_num_stateFE <- lm(num_insurers~num_hospitals+num_hospitals*year_dummy+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+state_abb*year_dummy+medicare_pc, data=full_combined)
-model_num_noFE <- lm(num_insurers~num_hospitals+num_hospitals*year_dummy+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+medicare_pc+mlr+state_govt+medicaid_expansion, data = full_combined)
+model_num_full <- lm(num_insurers~num_hospitals+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+ra_year.f+medicare_pc+year_dummy, data=subset(full_combined, single_county_RA==FALSE))
+model_num_stateFE <- lm(num_insurers~num_hospitals+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+st_year.f+year_dummy+medicare_pc+year_dummy, data=full_combined)
+model_num_noFE <- lm(num_insurers~num_hospitals+no_hospitals+white_popn_percent+black_popn_percent+native_popn_percent+poverty_rate+median_age+rucc_code_13+medicare_pc+mlr+state_govt+medicaid_expansion+year_dummy, data = full_combined)
 
 #Making diagnostic plots real quick
 library(ggplot2)
@@ -269,7 +273,7 @@ stargazer(model_full, model_stateFE, model_noFE, se = list(clust(model_full), cl
           type = "latex", column.labels = c("Main model", "Boozary et al. (2019)", "Griffith et al. (2018)"), font.size = "scriptsize", dep.var.labels = "Insurer HHI (logged)", 
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Main results", out = "../paper/tables/main_results.tex")
 
@@ -278,7 +282,7 @@ stargazer(model_num_full, model_num_stateFE, model_num_noFE, se = list(clust(mod
           dep.var.labels = "Number of insurers", 
           covariate.labels = c("Number of hospitals", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Main results", out = "../paper/tables/main_num_results.tex")
 
@@ -286,7 +290,7 @@ stargazer(model_full_13, model_full_46, model_full_79, se = list(clust(model_ful
           type = "latex", column.labels = c("RUCC 1-3", "RUCC 4-6", "RUCC 7-9"), font.size = "scriptsize", dep.var.labels = "Insurer HHI (logged)", 
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc", 
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc", 
                    "state_govt", "mlr", "medicaid_expansion"), 
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Results by rurality (main model)", out = "../paper/tables/main_results_rucc.tex")
@@ -297,7 +301,7 @@ stargazer(model_full_south, model_full_west, model_full_northcentral, model_full
           dep.var.labels = "Insurer HHI (logged)", 
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc",
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc",
                    "state_govt", "mlr", "medicaid_expansion"), 
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Results by region (main model)", out = "../paper/tables/main_results_region.tex")
@@ -306,7 +310,7 @@ stargazer(model_full_1, model_stateFE_1, model_noFE_1, se = list(clust(model_ful
           type = "latex", column.labels = c("Main model", "Boozary et al. (2019)", "Griffith et al. (2018)"), font.size = "scriptsize", dep.var.labels = "Insurer HHI (logged)", 
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"), 
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Main results (hospital market radius one SD lower)", out = "../paper/tables/main_results_1.tex")
 
@@ -314,7 +318,7 @@ stargazer(model_full_2, model_stateFE_2, model_noFE_2, se = list(clust(model_ful
           type = "latex", column.labels = c("Main model", "Boozary et al. (2019)", "Griffith et al. (2018)"), font.size = "scriptsize", dep.var.labels = "Insurer HHI (logged)", 
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Rurality (RUCC code)", "Year"), 
           omit = c("white_popn_percent", "black_popn_percent", "native_popn_percent", "rating_area.f", 
-                   "rating_area.f*year_dummy", "state_abb*year_dummy", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"),
+                   "ra_year.f", "st_year.f", "state_abb", "poverty_rate", "median_age", "medicare_pc", "state_govt", "mlr", "medicaid_expansion"),
           notes = c("Also controlling for rating area/year fixed effects, and county/state covariates where appropriate."), float = FALSE,
           title = "Main results (hospital market radius one SD higher)", out = "../paper/tables/main_results_2.tex")
 
@@ -323,7 +327,7 @@ stargazer(model_full_rurality, model_full_region, se=list(clust(model_full_rural
           covariate.labels = c("Hospital HHI (logged)", "No hospitals in market", "Year", "Hospital HHI (logged) * RUCC code 4-6", 
                                "Hospital HHI (logged) * RUCC code 7-9", "Hospital HHI (logged) * South", "Hospital HHI (logged) * North Central",
                                "Hospital HHI (logged) * West"), float = FALSE,
-          omit = c("white_popn_percent", "black_popn_percent", "rucc_code_13", "native_popn_percent", "rating_area.f", "year_dummy", "poverty_rate", "median_age", "medicare_pc"), 
+          omit = c("white_popn_percent", "black_popn_percent", "rucc_code_13", "native_popn_percent", "ra_year.f", "year_dummy", "poverty_rate", "median_age", "medicare_pc"), 
           notes = c("Also controlling for rating area/year fixed effects, and county covariates.", "Coefficient for Hospital HHI (logged) centered at RUCC codes 1-3 in (1), and Northeast region in (2)"),
           out = "../paper/tables/region_rurality_results.tex", title = "Results by rurality and region (main model)")
 
